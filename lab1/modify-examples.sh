590bd4400bc26339246c778ef02daf84eb5da40f
godotto <m.marcinkiewicz@protonmail.com>
2021-03-24 15:13:45 +0100

    Add more test cases

--- lab1/modify-examples.sh
+++ lab1/modify-examples.sh
@@ -68,4 +68,69 @@ echo TEST CASE 6
 read -p "Press enter to continue"
 clear
 
-rm -r testing-directory # tidy up testing directories
+# test case 7 - change letters to uppercase in one file
+echo TEST CASE 7
+./modify -u $test_dir/test1.txt
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 8 - change letters to uppercase in two files with special characters in names
+echo TEST CASE 8
+./modify -u $test_dir/test-dir1/test\*dir3/te\*st3.txt $test_dir/\!test-dir2/test\*dir3/te\ st7.txt $test_dir/test-dir1/test2.txt
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 9 - change letters to lowercase in one file
+echo TEST CASE 9
+./modify -l $test_dir/test-dir1/TEST2.TXT
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 10 - change directory name with special characters according to sed pattern
+echo TEST CASE 10
+./modify "s/-/=/g" $test_dir/\!test-dir2/test\ dir5/-test-dir6/
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 11 - run command with invalid sed pattern
+echo TEST CASE 11
+./modify "s/-/=" $test_dir/test-dir1/test\ dir5/-test-dir6/test8.txt
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 12 - run command with non-existing file
+echo TEST CASE 12
+./modify -l $test_dir/TEST9.TXT
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 13 - change letters into uppercase in a recursive way
+echo TEST CASE 13
+./modify -ru $test_dir/\!test-dir2
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 14 - change letters into lowercase in a recursive way
+echo TEST CASE 14
+./modify -rl $test_dir/\!TEST-DIR2/TEST\&DIR4
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# test case 15 - change names according to sed pattern in every file and directory (in a recursive way)
+echo TEST CASE 15
+./modify -r "s/test/passed-test/" $test_dir
+test_dir=$(echo $test_dir | sed "s/test/passed-test/")
+file_structure_command=$(echo $file_structure_command | sed "s/test/passed-test/")
+$file_structure_command # print file structure
+read -p "Press enter to continue"
+clear
+
+# rm -r $test_dir # tidy up testing directories
