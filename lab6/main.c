acc13800dc0db056c0f0f1f6e39fb605f43d7184
godotto <m.marcinkiewicz@protonmail.com>
2021-06-14 20:57:09 +0200

    Some very basic error handling

--- lab6/main.c
+++ lab6/main.c
@@ -28,6 +28,12 @@ void copy_read_write(int fd_from, int fd_to)
     // 0 in num_of_bytes indicates EOF
     while (num_of_bytes != 0)
     {
+        if (num_of_bytes == -1)
+        {
+            printf("Error while reading the file\n");
+            exit(EXIT_FAILURE);
+        }
+
         write(fd_to, buf, num_of_bytes);
         num_of_bytes = read(fd_from, buf, BUFFER_SIZE);
     }
@@ -39,6 +45,12 @@ void copy_mmap(int fd_from, int fd_to)
     // get the size of the file to copy
     off_t file_size = lseek(fd_from, (off_t)0, SEEK_END);
 
+    if (file_size == -1)
+    {
+        printf("Error while reading the size of file\n");
+        exit(EXIT_FAILURE);
+    }
+
     // map file to copy into memory
     void *address_from = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd_from, (off_t)0);
 
@@ -46,6 +58,12 @@ void copy_mmap(int fd_from, int fd_to)
     lseek(fd_to, file_size - 1, SEEK_SET);
     write(fd_to, "", 1);
     void *address_to = mmap(NULL, file_size, PROT_WRITE, MAP_SHARED, fd_to, (off_t)0);
+
+    if (address_from == -1 || address_to == -1)
+    {
+        printf("Error while mapping the file\n");
+        exit(EXIT_FAILURE);
+    }
 
     // copy memory
     memcpy(address_to, address_from, file_size);
